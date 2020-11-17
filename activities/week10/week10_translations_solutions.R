4e2b7155e84674f3b982d729a6b6d7b3d14a0cbc
James Henderson <jbhender@umich.edu>
2020-11-17 11:41:58 -0500

    Solutions for Week 10 activity.

--- activities/week10/week10_translations_solutions.R
+++ activities/week10/week10_translations_solutions.R
@@ -58,7 +58,7 @@ batting_tbl %>%
 # SQL
 query = 
 '
-SELECT playerID, yearID, lgID as league
+SELECT playerID, yearID, lgID as league,
        (Cast (H as Float) /  Cast (AB as Float) ) as avg
 FROM BATTING
 '
@@ -67,11 +67,11 @@ lahman %>% tbl(sql(query)) %>% collect()
 # Note: R will handle the conversion of integers to numeric. 
 
 # data.table (compute in "j")
-batting_dt[, .(playerID, avg = H / AB)]
+batting_dt[, .(playerID, yearID, league = lgID, avg = H / AB)]
 
 # dplyr 
 batting_tbl %>%
-  transmute(playerID, avg = H / AB)
+  transmute(playerID, yearID, league = lgID, avg = H / AB)
 
 ## Question 3 #################################################################
 # compute the maximum HBP in a "stint" (player season with a single team)
@@ -81,11 +81,13 @@ batting_tbl %>%
   summarize(max_HBP = max(HBP, na.rm = TRUE))
 
 # data.table (summarize in "j")
+batting_dt[, .(max_HBP = max(HBP, na.rm = TRUE))]
 
 # SQL
 query = 
 '
-# write SQL query here
+SELECT max(HBP) as max_hbp
+FROM BATTING
 '
 lahman %>% tbl(sql(query)) 
 
@@ -98,11 +100,17 @@ batting_dt[ , .(avg = sum(H) / sum(AB)), .(playerID)]
 # SQL, cast integers to numeric following example in question 2
 query = 
 '
-# write SQL query here
+SELECT playerID, yearID, lgID,
+       ( Cast (sum(H) as Float) / Cast (sum(AB) as Float) ) as avg
+FROM BATTING
+GROUP BY playerID, yearID, lgID
 '
 lahman %>% tbl(sql(query)) 
 
 # dplyr
+batting_tbl %>% 
+  group_by(playerID, yearID, lgID) %>%
+  summarize( avg = sum(H) / sum(AB) )
 
 ## Question 5 #################################################################
 # select rows for year 2016
@@ -113,11 +121,14 @@ batting_tbl %>%
   select(playerID, HBP)
 
 # data.table (subset in i)
+batting_dt[yearID == 2016, .(playerID, HBP)]
 
 # SQL
 query = 
 '
-# write SQL query here
+SELECT playerID, HBP
+FROM BATTING
+WHERE yearID = 2016
 '
 lahman %>% tbl(sql(query)) 
 
@@ -141,14 +152,26 @@ ORDER BY -HR
 lahman %>% tbl(sql(query)) %>% collect()
 
 # data.table (use %>% with .[], or chaining dt[][])
+batting_dt[ yearID > 1999, .(HR = sum(HR)), .(playerID)] %>%
+  .[HR > 400]
 
 # dplyr
+batting_tbl %>% 
+  filter(yearID > 1999) %>%
+  group_by(playerID) %>%
+  summarize(HR = sum(HR)) %>%
+  filter(HR > 400)
 
 ## Question 7 #################################################################
 # final the number of "20-20" seasons with 20+ SB and 20+ HR by a 
 # player (across all stints) in each year, 2000-2019 since 2000
 
 # SQL: 20-20
+#! original query: misses cases where sum(SB) > 19 and sum(HR) > 19 but either
+#   condition does not hold on a single row.  To fix, we need the aggregate
+#   function the having statement. 
+# See: https://www.w3schools.com/sql/sql_having.asp
+
 query = 
 '
 SELECT yearID, COUNT(playerID) as N
@@ -163,10 +186,54 @@ GROUP BY yearID
 '
 lahman %>% tbl(sql(query)) %>% collect()
 
-# data.table: 20-20
+query2 = 
+'
+SELECT yearID, COUNT(playerID) as N
+FROM (
+ SELECT playerID, yearID, sum(SB) as SB, sum(HR) as HR
+ FROM BATTING
+ WHERE yearID > 1999
+ GROUP BY playerID, yearID
+ HAVING sum(SB) > 19 AND sum(HR) > 19
+)
+GROUP BY yearID
+'
+lahman %>% tbl(sql(query2)) %>% collect()
 
-# dplyr
+#! Here is an example case we miss from 2002 without the aggregates in the
+#  having clause. 
+batting_dt[ playerID == 'encarju01' & yearID == 2002]
 
+# data.table: 20-20 (query 2 translation)
+batting_dt[
+  yearID > 1999, 
+  .(SB = sum(SB), HR = sum(HR)), 
+  .(playerID, yearID)
+] %>%
+  .[SB > 19 & HR > 19] %>% 
+  .[, .N, yearID]
+
+batting_dt[
+  yearID > 1999, 
+  .(SB = sum(SB), HR = sum(HR)), 
+  .(playerID, yearID)
+] %>%
+  .[SB > 19 & HR > 19 & yearID == 2002]
+
+
+batting_dt[yearID > 1999, 
+           .(SB = sum(SB), HR = sum(HR)), 
+           .(playerID, yearID)
+][SB > 19 & HR > 19
+][, .N, yearID]
+
+# dplyr (query 2 translation)
+batting_tbl %>%
+  filter(yearID > 1999) %>%
+  group_by(yearID, playerID) %>%
+  summarize( SB = sum(SB), HR = sum(HR) ) %>%
+  filter( SB > 19 & HR > 19) %>%
+  summarize(n = n())
 
 # 79: -------------------------------------------------------------------------
 
